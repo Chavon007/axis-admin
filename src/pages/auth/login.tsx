a44f1fdfeb6e3735ace5d9b1244a2252a8352dd5
azuh salvation <salvationazuh@gmail.com>
2026-04-13 22:18:02 +0100

    added the login page yet to style it

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom";
+import useAuth from "../../hooks/authHooks";
+import React, { useState } from "react";
+import type { LoginDetails } from "../../types/auth";
 
 function LoginPage() {
+  const { login, loading, error, success } = useAuth();
+
+  const [formData, setFormData] = useState<LoginDetails>({
+    email: "",
+    password: "",
+  });
+  const [remember, setRemember] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await login(formData);
+  };
   return (
     <div>
       <div>
@@ -8,14 +22,17 @@ function LoginPage() {
           <img src="" alt="" />
           <h3>Sign in</h3>
           <small>Hotel management portal</small>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label>
               Email
               <input
                 type="email"
                 name="email"
-                value={}
+                value={formData.email}
                 placeholder="Enter email address"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
               />
             </label>
 
@@ -24,23 +41,33 @@ function LoginPage() {
               <input
                 type="password"
                 name="password"
-                value={}
+                value={formData.password}
                 placeholder="********"
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
               />
             </label>
 
             <div>
               <label>
                 {" "}
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  checked={remember}
+                  onChange={(e) => setRemember(e.target.checked)}
+                />
                 Remember me
               </label>
 
               <Link to="">Forget password?</Link>
             </div>
 
-            <button> Sign In</button>
-
+            <button type="submit" disabled={loading}>
+              {loading ? "logging in..." : "Login"}
+            </button>
+            {error && <p>{error}</p>}
+            {success && <p>{success}</p>}
             <p>
               <span>Don't have an account?</span>
               <span>
@@ -51,6 +78,11 @@ function LoginPage() {
             <Link to="">Super Admin access - Super admin panel</Link>
           </form>
         </article>
+
+        {/* image */}
+        <div>
+          <img src="" alt="axis logo" />
+        </div>
       </div>
     </div>
   );
