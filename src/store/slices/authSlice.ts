f8a3b5bc5c5812065a99164358e720b824ac33a1
azuh salvation <salvationazuh@gmail.com>
2026-04-13 20:09:34 +0100

    added redux toolkit for authslice

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { SignupDetails } from "../../types/auth";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+  user: SignupDetails | null;
+  token: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (
+      state,
+      action: PayloadAction<{ user: SignupDetails; token: string }>,
+    ) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    clearUser: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
